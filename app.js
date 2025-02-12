4bb7784695d0a9f706444dc176f5663f57667aba
smitavh1 <smitavh@gmail.com>
2025-02-12 17:07:25 +1000

    Pushing to Git Remote

---
+++ app.js
@@ -0,0 +1,21 @@
+let myAge = 20;
+//This is my age;
+let earlyYears = 2;
+//earlyYears are calcualted at 10.5 each.
+earlyYears = earlyYears * 10.5;
+laterYears = myAge - 2;
+// subtracting the first 2 years.
+let dogYears = laterYears * 4;
+dogYears = dogYears + laterYears;
+//myAgeInDogYears
+//This has laterYears of dog.
+console.log(`${dogYears} and ${laterYears}`);
+
+let myName = 'Smita Hodiggeri';
+myName = myName.toLowerCase();
+//Converting this into a lower case
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${dogYears} years old in dog years.
+`);
+
+
+
